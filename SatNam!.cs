90ed7684bdf185947888f29168deb75ed9d1a9a3
Opilane TTHK <opilane@tthk.ee>
2019-09-06 17:03:25 +0300

    MyFirstCommit

---
+++ SatNam!.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SatNam");
+            Console.WriteLine("Mis on sinu nimi?");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("SatNam, " + name)
+
+        }
+    }
+}
